17fcc32abf796cb6958a4370265f9c859e382ef4
tv-abhinav <abhinavtv97@gmail.com>
2019-07-27 12:23:24 +0530

    Data Cleaned

--- tsaKernel.py
+++ tsaKernel.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import 
+import numpy as np
 
 trainCSV = pd.read_csv("train.csv", encoding="ISO-8859-1")
 df = pd.DataFrame(trainCSV)
-print(pd.isnull(df).any())
-plt.matshow(df.corr())
-plt.show()
+df = df.drop(['ItemID'], axis=1)
+column_names=["SentimentText","Sentiment"]
+df=df.reindex(columns=column_names)
+df.rename(columns={'SentimentText':'Tweet','Sentiment':'Result'},inplace=True)
+print(np.count_nonzero(df.duplicated(subset="Tweet", keep='first')))
+print(df.head(5))
